17ea8977a79b6b913164d3ed4391399aa6cb6442
David Adams <david.riding.rails@gmail.com>
2019-02-20 14:03:16 -0700

    Add messages controller methods & filters

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,36 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: :show
+  before_action :logged_in?, only: [:new, :create, :show]
+
   def index
+    @messages = Message.all
   end
 
   def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:success] = "A new message was added"
+      redirect_to messages_path
+    else
+      flash[:danger] = "Message was not added, try again"
+      render :new
+    end
   end
 
   def show
   end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:title, :body, :user_id)
+    end
+
+    def set_message
+      @message = Message.find(params[:id])
+    end
 end
