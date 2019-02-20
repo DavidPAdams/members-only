3b0405665692366b00f073b09f6d97aacab91658
David Adams <david.riding.rails@gmail.com>
2019-02-19 17:42:46 -0700

    Work up User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,44 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :remember_digest
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  before_save :downcase_email
+  before_create :generate_remember_digest
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
   has_secure_password
+
+  validates :password, presence: true, length: { minimum: 8 }
+
+  has_many :messages
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(string)
+    Digest::SHA1.hexdigest(string)
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  private
+
+    def downcase_email
+      email.downcase!
+    end
+
+    def generate_remember_digest
+      self.remember_token = User.new_token
+      self.remember_digest = User.digest(remember_token)
+    end
+
 end
