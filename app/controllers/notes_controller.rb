5974b995a3b58a7f6b12daab4311cb6825503c61
David Adams <david.riding.rails@gmail.com>
2019-02-20 17:41:34 -0700

    Add notes model

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,36 @@
+class NotesController < ApplicationController
+  before_action :set_note, only: :show
+  before_action :logged_in?, only: [:new, :create, :show]
+
+  def index
+    @notes = Note.all
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      flash[:success] = "A new message was added"
+      redirect_to notes_path
+    else
+      flash[:danger] = "Message was not added, try again"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+    def note_params
+      params.require(:note).permit(:title, :body, :user_id)
+    end
+
+    def set_message
+      @note = Note.find(params[:id])
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_003041) do
+ActiveRecord::Schema.define(version: 2019_02_21_003018) do
 
   create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
