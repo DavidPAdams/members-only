7a1e919133d8c1cb9184d347a750b4570323f8f7
David Adams <david.riding.rails@gmail.com>
2019-02-20 17:40:48 -0700

    Swap out messages model

--- app/models/message.rb
+++
@@ -1,6 +0,0 @@
-class Message < ApplicationRecord
-  belongs_to :user
-
-  validates :title, presence: true, length: { maximum: 100 }
-  validates :body, presence: true, length: { maximum: 1000 }
-end
