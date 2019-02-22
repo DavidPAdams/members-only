f31ccda637c51c6996495672392e70647dd78376
David Adams <david.riding.rails@gmail.com>
2019-02-22 09:10:53 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_02_21_003018) do
 
-  create_table "messages", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "body"
